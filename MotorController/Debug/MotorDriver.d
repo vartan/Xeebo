13cea0dc2a6601d5a5c6e9eafa9364b12e6baadd
vartan <michael.vartan@student.csulb.edu>
2015-04-21 17:22:36 -0700

    more fixing

--- MotorController/Debug/MotorDriver.d
+++
@@ -1,8 +0,0 @@
-MotorDriver.o MotorDriver.d: ../MotorDriver.c ../globals.h \
- ../MotorDriver.h ../timers.h
-
-../globals.h:
-
-../MotorDriver.h:
-
-../timers.h:
--- MotorController/Debug/cr_startup_lpc11u.d
+++
@@ -1,4 +0,0 @@
-cr_startup_lpc11u.d: ../cr_startup_lpc11u.c \
- /Users/vartan/LPCXpresso/workspace/CMSISv2p00_LPC11Uxx/inc/system_LPC11Uxx.h
-
-/Users/vartan/LPCXpresso/workspace/CMSISv2p00_LPC11Uxx/inc/system_LPC11Uxx.h:
