69c88ef4c2e2c09172037179434db2e8d376a45b
vartan <michael.vartan@student.csulb.edu>
2015-05-05 15:31:59 -0700

    added server

---
+++ Server/Server.js
@@ -0,0 +1,212 @@
+#!/usr/bin/env node
+"use strict";
+var Q = require("q");
+// Open a new message handler
+var serialIdentifier = process.argv[2] || "/dev/ttyUSB0";
+var messageHandler = require("serialmessages")(serialIdentifier);
+
+var messageList = getMessageProperties();
+
+// echo all received characters back to command line
+messageHandler.serialPort.on("data", echo(messageHandler.serialPort));
+
+for(var message_name in messageList) {
+  var mp = messageList[message_name];
+  mp.messageHandler = messageHandler.addHandler({
+    id:     mp.id,
+    name:   mp.name,
+    length: mp.receiveLength
+  });
+  if(mp.receiveHandler !== null) {
+    mp.messageHandler.on("message", mp.receiveHandler);
+  }
+
+}
+// Add message with ID 0x61 (ASCII 'a'). 
+var testHandler = messageHandler.addHandler({
+  id:       ascii('a'),
+  name:     "'a' handler", 
+  length:   4, 
+}).on("message", logMessageHandler).on("message", sendNewLine);
+
+// Add message with ID 0x62 (ASCII 'b')
+var testHandler2 = messageHandler.addHandler({
+  id:       ascii('b'), 
+  name:     "'b' handler",
+  length:   5, 
+}).on("message", logMessageHandler).on("message", sendNewLine);
+
+// Add message with ID 0x65 (ascii 'e')
+var testHandler3 = messageHandler.addHandler({
+  id:       ascii('e'), 
+  name:     "exit handler",
+  length:   3, 
+}).on("message", sendNewLine);
+
+// Add to message handler, if the payload is "xit", then send a newline, then
+// exit the program.
+testHandler3.on("message", function(event) {
+  if (event.data.toString() === "xit") {
+    Q.fcall(messageHandler.sendMessage("Exiting...\r\n"))
+    .delay(10)
+    .then(process.exit);
+
+  }
+});
+
+
+
+
+//Example sequence of events:
+//1. ensure message handler is open
+//2. send "test" message
+//3. report how many characters were sent 
+//4. receive 'a' message
+//5. console log a message back to user
+//6. receive 'b' message.
+//7. console log b message back to user
+//8. log "finished"
+
+Q.when(messageHandler.open())                                                //1
+  .then(messageHandler.sendMessage("test\r\n"))                              //2
+  .then(function handleSentMessage(messageLength) {                          //3
+    console.log("sent message which was "+messageLength+" characters long.");
+  })
+  .then(testHandler.receive())                                               //4
+  .then(function handleFirstMessage(event) {                                 //5
+    console.log("Finally received expected message 1: "+event.data);               
+  })
+  .then(testHandler2.receive())                                              //6
+  .then(function handleSecondMessage(event) {                                //7
+    console.log("Finally received expected message 2: "+event.data);            
+  })  
+  .finally(function logFinished() {                                          //8
+    console.log("Finished!");
+  })
+  .catch(function(error){console.log(error);});
+
+
+
+/*************************** Misc. Functions Below ***************************/
+
+/**
+ * Log received messages to command line, and notify user via bell.
+ * @param  {Buffer} event Event received over serial
+ */
+function logMessageHandler(event) {
+  console.log("(" + event.messageHandler.name + ": " + event.data + ")\u0007");
+}
+
+/**
+ * Used to echo received data back over serial.
+ * @param  {Buffer} serialPort SerialPort to echo to
+ */
+function echo(serialPort) {
+  /**
+   * Echo to serial port
+   * @param  {Buffer} data Data to send over port
+   */
+  return function echoToPort(data) {
+    var i;
+    for (i = 0; i < data.length; i += 1) {
+      var char = data[i];
+      if (char === "\r".charCodeAt(0)) {
+        serialPort.write("\r\n", function(){});
+      } else if (char === 0x7f) {
+        serialPort.write("\b \b", function(){});
+      } else {
+        serialPort.write(String.fromCharCode(char),function(){});
+      }
+    }
+  };
+}
+/**
+ * Send New Line
+ * Sends a line break over serial, for debugging purposes
+ * @return {Promise} Promise to send new newline character
+ */
+function sendNewLine() {
+  return Q.fcall(messageHandler.sendMessage("\r\n")); 
+}
+
+function ascii (char){
+  return char.charCodeAt(0);
+}
+
+function getMessageProperties() {
+  return {
+    /** 
+     * Do nothing; Send (MAX(num bytes)) times to flush out messages if out of sync.
+     * 
+     * @property FLUSH
+     */
+    FLUSH: {
+      id:            0x00,
+      name:          "FLUSH",
+      receiveLength: 0,
+      sendLength:    0,
+      receiveHandler: null,
+    },
+    /** 
+     * Send a ping to verify that the line is valid
+     * 
+     * @property PING
+     */
+    PING: {
+      id:            0x01,
+      name:          "PING",
+      receiveLength: 1,
+      sendLength:    1,
+      receiveHandler: null,
+    },
+    /** 
+     * Reply to a ping with a pong
+     * 
+     * @property PONG
+     */
+    PONG: {
+      id:            0x02,
+      name:          "PONG",
+      receiveLength: 1,
+      sendLength:    1,
+      receiveHandler: null,
+    },
+    /** 
+     * Update the motion vectors to move the robot
+     * 
+     * @property NEW_MOTION
+     */
+    NEW_MOTION: {
+      id:            0x03,
+      name:          "NEW_MOTION",
+      receiveLength: 0,
+      sendLength:    6,
+      receiveHandler: null,
+    },
+    /** 
+     * Request the motion vector state controlling the motors
+     * 
+     * @property GET_MOTION
+     */
+    GET_MOTION: {
+      id:            0x04,
+      name:          "GET_MOTION",
+      receiveLength: 6,
+      sendLength:    0,
+      receiveHandler: null,
+    },
+    /** 
+     * Request the current battery life
+     * 
+     * @property GET_BATTERY
+     */
+    GET_BATTERY: {
+      id:            0x05,
+      name:          "GET_BATTERY",
+      receiveLength: 1,
+      sendLength:    0,
+      receiveHandler: null,
+    },
+  };
+}
+
